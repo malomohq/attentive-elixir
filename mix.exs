5263f2dafd6cb48466b678e35a86918de50c3c93
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-07-01 17:03:18 -0400

    Added dialyxir and ex_doc as dev dependencies

--- mix.exs
+++ mix.exs
@@ -18,6 +18,10 @@ defmodule Attentive.MixProject do
   end
 
   defp deps do
-    []
+    [
+      { :dialyxir, "~> 1.0", only: :dev, runtime: false },
+
+      { :ex_doc, ">= 0.0.0", only: :dev, runtime: false }
+    ]
   end
 end
