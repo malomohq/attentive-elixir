3374670843838c3025179969eaf64eaa872faf7f
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-07-02 09:52:53 -0400

    Added types for http stuff

--- lib/attentive.ex
+++ lib/attentive.ex
@@ -1,2 +1,10 @@
 defmodule Attentive do
+  @type http_headers_t ::
+          [{ String.t(), String.t() }]
+
+  @type http_method_t ::
+          :delete | :get | :head | :patch | :post | :put
+
+  @type http_status_code_t ::
+          post_integer
 end
