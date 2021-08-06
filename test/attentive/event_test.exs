9f641a9d2b3762e72feda4a81b29042c12b83bfc
Anthony Smith <420061+anthonator@users.noreply.github.com>
2021-08-06 13:57:15 -0400

    Renamed Attentive.Events to Attentive.Event

--- test/attentive/event_test.exs
+++ test/attentive/event_test.exs
@@ -9,7 +9,7 @@ defmodule Attentive.EventTest do
     expected = Map.put(expected, :params, [p1: "v"])
     expected = Map.put(expected, :path, "/events/ecommerce/add-to-cart")
 
-    assert expected == Attentive.Events.add_to_cart(p1: "v")
+    assert expected == Attentive.Event.add_to_cart(p1: "v")
   end
 
   test "create/1" do
@@ -18,7 +18,7 @@ defmodule Attentive.EventTest do
     expected = Map.put(expected, :params, [p1: "v"])
     expected = Map.put(expected, :path, "/events/custom")
 
-    assert expected == Attentive.Events.create(p1: "v")
+    assert expected == Attentive.Event.create(p1: "v")
   end
 
   test "product_view/1" do
@@ -27,7 +27,7 @@ defmodule Attentive.EventTest do
     expected = Map.put(expected, :params, [p1: "v"])
     expected = Map.put(expected, :path, "/events/ecommerce/product-view")
 
-    assert expected == Attentive.Events.product_view(p1: "v")
+    assert expected == Attentive.Event.product_view(p1: "v")
   end
 
   test "purchase/1" do
@@ -36,6 +36,6 @@ defmodule Attentive.EventTest do
     expected = Map.put(expected, :params, [p1: "v"])
     expected = Map.put(expected, :path, "/events/ecommerce/purchase")
 
-    assert expected == Attentive.Events.purchase(p1: "v")
+    assert expected == Attentive.Event.purchase(p1: "v")
   end
 end
